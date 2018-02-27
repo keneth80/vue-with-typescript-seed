479a643fc4d95e51f41df2f8a42e2b8164f54b2d
강효선 <kanghyoseon@ganghyoseon-ui-MacBook-Pro.local>
2018-02-27 12:43:50 +0900

    first commit

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -6,49 +6,44 @@
 </template>
 
 <script lang="ts">
-export default {
-  name: 'hello',
-  data () {
-    return {
-      message: 'Welcome to Your Vue.js App'
-    }
-  },
+import Vue from 'vue';
+import Component from 'vue-class-component';
 
-  computed: {
-    fullMessage(){
-      return `${this.message} from Typescript`;
-    }
-  },
+@Component({})
+export default class Hello extends Vue {
+    message: string = 'Hello Kenneth!';
 
-  created() {
-    console.log('created');
-  },
-
-  methods: {
-    clicked(){
-      console.log('clicked');
+    get fullMessage() {
+        return `${this.message} from Typescript`;
     }
-  }
+
+    created() {
+        console.log('created');
+    }
+
+    clicked() {
+        console.log('clicked');
+    }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h1, h2 {
-  font-weight: normal;
+    font-weight: normal;
 }
 
 ul {
-  list-style-type: none;
-  padding: 0;
+    list-style-type: none;
+    padding: 0;
 }
 
 li {
-  display: inline-block;
-  margin: 0 10px;
+    display: inline-block;
+    margin: 0 10px;
 }
 
 a {
-  color: #42b983;
+    color: #42b983;
 }
 </style>
